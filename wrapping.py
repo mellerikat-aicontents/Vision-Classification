0f8c12bab3d5c52925053c983c9ac99e9ab22855
서윤지 <yoonji.suh@lge.com>
2023-11-24 00:00:11 +0000

    Alo update

--- wrapping.py
+++ wrapping.py
@@ -1,40 +1,60 @@
 import argparse
 import time
 import os
-os.chdir(os.path.abspath(os.path.join('./alo')))
+if os.path.abspath('.').split('/')[-1]!='alo':
+    os.chdir(os.path.abspath(os.path.join('./alo')))
 from src.alo import ALO
 from src.alo import AssetStructure
 from src.external import external_load_data, external_save_artifacts
 import pickle
 from glob import glob
-import seaborn as sns
-from sklearn.metrics import classification_report, confusion_matrix
-import pandas as pd
-import matplotlib.pyplot as plt
-import missingno as msno
-import tensorflow as tf
-from PIL import Image
-import numpy as np
-import copy
-
+import_libraries = False
+try:
+    import seaborn as sns
+    from sklearn.metrics import classification_report, confusion_matrix
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import missingno as msno
+    import tensorflow as tf
+    from PIL import Image
+    import numpy as np
+    import copy
+    import_libraries = True
+except:
+    pass
 
 EVAL_PATH = './evaluation_data'
 PIPELINE_DICT = {'train_pipeline':'train', 'inference_pipeline':'inference'}
 CARDINALITY_LIMIT = 20
 
 class Wrapper(ALO):
-    def __init__(self, nth_pipeline, eval_report=True):
+    def __init__(self, nth_pipeline, exp_plan_file='experimental_plan.yaml', eval_report=True):
+        global import_libraries
         super().__init__()
+        self.exp_plan_file = exp_plan_file
+
         self.set_proc_logger()
         self.preset()
+        self.set_asset_structure()
+        if not import_libraries:
+            import seaborn as sns
+            from sklearn.metrics import classification_report, confusion_matrix
+            import pandas as pd
+            import matplotlib.pyplot as plt
+            import missingno as msno
+            import tensorflow as tf
+            from PIL import Image
+            import numpy as np
+            import copy
+            import_libraries = True
+
         pipelines = list(self.asset_source.keys())
         self.pipeline = pipelines[nth_pipeline]
         self.eval_report = eval_report
         
         external_load_data(pipelines[nth_pipeline], self.external_path, self.external_path_permission, self.control['get_external_data'])
         self.install_steps(self.pipeline, self.control["get_asset_source"])
-        envs, args, data, config = {}, {}, {}, {}
-        self.asset_structure = AssetStructure(envs, args, data, config)
+        
         self.set_proc_logger()
         self.step = 0
         self.args_checker = 0
@@ -44,9 +64,9 @@ class Wrapper(ALO):
         
         
     def get_args(self, step=None):
-        if step is None:
-            step = self.step
-        self.args = super().get_args(self.pipeline, step)
+        if step is not None:
+            self.step = step
+        self.args = super().get_args(self.pipeline, self.step)
         self.asset_structure.args = self.args
         self.args_checker = 1
 
@@ -56,14 +76,14 @@ class Wrapper(ALO):
         if self.args_checker==0:
             self.get_args()
         
-        if step is None:
-            step = self.step
+        if step is not None:
+            self.step = step
         if args is not None:
             self.asset_structure.args = args
         if data is not None:
             self.asset_structure.data = data
         
-        self.asset_structure = self.process_asset_step(self.asset_source[self.pipeline][step], step, self.pipeline, self.asset_structure)
+        self.asset_structure = self.process_asset_step(self.asset_source[self.pipeline][self.step], self.step, self.pipeline, self.asset_structure)
         
         self.data = self.asset_structure.data
         self.args = self.asset_structure.args
